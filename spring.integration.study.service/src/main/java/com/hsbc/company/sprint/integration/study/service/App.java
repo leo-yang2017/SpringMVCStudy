de04f73e54e27389bbfe48d2965e7f935c5d0727
Leo_yang <875745136@qq.com>
2017-04-25 19:17:28 +0800

    init spring integration study repository

---
+++ spring.integration.study.service/src/main/java/com/hsbc/company/sprint/integration/study/service/App.java
@@ -0,0 +1,13 @@
+package com.hsbc.company.sprint.integration.study.service;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
